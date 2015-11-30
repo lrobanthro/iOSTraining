7d5d3e190a50c390292158b746386726fc897b7c
lrobanthro <lrobinson@anthropologie.com>
2015-11-30 13:32:05 -0500

    added Vehicle and Car classes

---
+++ iOS Training/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  iOS Training
+//
+//  Created by Lynne Robinson on 11/24/15.
+//  Copyright © 2015 URBN. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@end
---
+++ iOS Training/Vehicle.m
@@ -0,0 +1,24 @@
+//
+//  Vehicle.m
+//  iOS Training
+//
+//  Created by Lynne Robinson on 11/24/15.
+//  Copyright © 2015 URBN. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@implementation Vehicle
+
+- (void)setupVehicleValues {
+    self.make = nil;
+    self.model = nil;
+    self.name = nil;
+    
+    self.numberOfDoors = 0;
+    self.numberOfWheels = 0;
+    
+    self.runsOnGas = NO;
+}
+
+@end
