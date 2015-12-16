9c77693ea85b376d36618a08c667dcf81ac55544
lrobanthro <lrobinson@anthropologie.com>
2015-12-16 15:56:50 -0500

    added label and button to viewcontroller

--- iOS Training/ViewController.m
+++ iOS Training/ViewController.m
@@ -16,9 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(20.0, 50.0, 200.0, 50.0)];
+    lbl.text = @"Press a button to enter some text";
+    lbl.backgroundColor = [UIColor lightGrayColor];
+    [lbl sizeToFit ];
+    [self.view addSubview:lbl];
+    
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [btn1 setTitle:@"Text 1" forState:UIControlStateNormal];
+    [btn1 addTarget:self action:@selector(btn1Touched:) forControlEvents:UIControlEventTouchUpInside];
+    [btn1 sizeToFit];
+    btn1.center = self.view.center;
+    [self.view addSubview:btn1];
 }
 
+- (void)btn1Touched:(id)sender {
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
