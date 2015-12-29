a50cef7362d8f7cffee61810362b20ffae6df095
lrobanthro <lrobinson@anthropologie.com>
2015-12-29 10:18:09 -0500

    changes to fortune teller

--- iOS Training/ViewController.m
+++ iOS Training/ViewController.m
@@ -21,6 +21,8 @@
     
     self.lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, 200, 20)];
     self.lbl.textColor = [UIColor grayColor];
+    self.lbl.numberOfLines = 0;
+    self.lbl.lineBreakMode = NSLineBreakByWordWrapping;
     [self.view addSubview:self.lbl];
     self.lbl.text = @"Click any of the buttons below for your fortune";
     [self.lbl sizeToFit];
@@ -30,8 +32,14 @@
     btn1.layer.cornerRadius = 3;
     btn1.layer.borderWidth = 1;
     btn1.layer.borderColor = [UIColor grayColor].CGColor;
-    btn1.tag = 1;
+    btn1.tag = 0;
     [btn1 sizeToFit];
+    
+    CGRect frame = btn1.frame;
+    frame.size.width = frame.size.width + 20.0;
+    frame.size.height = frame.size.height + 20.0;
+    btn1.frame = frame;
+    
     btn1.center = self.view.center;
     [self.view addSubview:btn1];
     [btn1 addTarget:self action:@selector(buttonTouched:) forControlEvents:UIControlEventTouchUpInside];
@@ -42,7 +50,7 @@
     btn2.layer.cornerRadius = 3;
     btn2.layer.borderWidth = 1;
     btn2.layer.borderColor = [UIColor grayColor].CGColor;
-    btn2.tag = 2;
+    btn2.tag = 1;
     [btn2 sizeToFit];
     btn2.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn1.frame) + 20);
     [self.view addSubview:btn2];
@@ -54,7 +62,7 @@
     btn3.layer.cornerRadius = 3;
     btn3.layer.borderWidth = 1;
     btn3.layer.borderColor = [UIColor grayColor].CGColor;
-    btn3.tag = 3;
+    btn3.tag = 2;
     [btn3 sizeToFit];
     btn3.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn2.frame) + 20);
     [self.view addSubview:btn3];
@@ -63,13 +71,15 @@
 
 - (void)buttonTouched:(id)sender {
     UIButton *btn = (UIButton*) sender;
-    NSString *labelText = [[NSString alloc] initWithString:btn.titleLabel.text];
-    NSArray *fortunes = @[@": Take advantage of an opportunity", @": Never quit!", @": Tomorrow is another day"];
-    NSString *fortune = fortunes[btn.tag - 1];
-    NSLog(@"Button Pressed!");
-    labelText = [labelText stringByAppendingString: fortune];
-    self.lbl.text = labelText;
+    NSString *labelText = btn.titleLabel.text;
+    NSArray *fortunes = @[@"Take advantage of an opportunity", @"Never quit!", @"Tomorrow is another day"];
+    NSString *fortune = fortunes[btn.tag];
+    self.lbl.text = [NSString stringWithFormat:@"%@: %@", labelText, fortune];
     [self.lbl sizeToFit];
+    
+    CGRect frame = self.lbl.frame;
+    frame.size.width = 320.0;
+    self.lbl.frame = frame;
 }
 
 - (void)didReceiveMemoryWarning {
