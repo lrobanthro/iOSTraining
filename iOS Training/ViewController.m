ef31337010b4b1325a7b929a854f43658fe8749d
lrobanthro <lrobinson@anthropologie.com>
2015-12-29 13:40:48 -0500

    changed buttons, added more lines to fortune 1

--- iOS Training/ViewController.m
+++ iOS Training/ViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.lbl = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, 200, 20)];
+    self.lbl = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 400, 20)];
     self.lbl.textColor = [UIColor grayColor];
     self.lbl.numberOfLines = 0;
     self.lbl.lineBreakMode = NSLineBreakByWordWrapping;
@@ -31,7 +31,7 @@
     [btn1 setTitle:@"Fortune #1" forState:UIControlStateNormal];
     btn1.layer.cornerRadius = 3;
     btn1.layer.borderWidth = 1;
-    btn1.layer.borderColor = [UIColor grayColor].CGColor;
+    btn1.layer.borderColor = [UIColor blueColor].CGColor;
     btn1.tag = 0;
     [btn1 sizeToFit];
     
@@ -49,10 +49,16 @@
     [btn2 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
     btn2.layer.cornerRadius = 3;
     btn2.layer.borderWidth = 1;
-    btn2.layer.borderColor = [UIColor grayColor].CGColor;
+    btn2.layer.borderColor = [UIColor redColor].CGColor;
     btn2.tag = 1;
     [btn2 sizeToFit];
-    btn2.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn1.frame) + 20);
+    
+    CGRect frame2 = btn2.frame;
+    frame2.size.width = frame2.size.width + 20.0;
+    frame2.size.height = frame2.size.height + 20.0;
+    btn2.frame = frame2;
+    
+    btn2.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn1.frame) + 50);
     [self.view addSubview:btn2];
     [btn2 addTarget:self action:@selector(buttonTouched:) forControlEvents:UIControlEventTouchUpInside];
     
@@ -64,7 +70,13 @@
     btn3.layer.borderColor = [UIColor grayColor].CGColor;
     btn3.tag = 2;
     [btn3 sizeToFit];
-    btn3.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn2.frame) + 20);
+    
+    CGRect frame3 = btn3.frame;
+    frame3.size.width = frame3.size.width + 20.0;
+    frame3.size.height = frame3.size.height + 20.0;
+    btn3.frame = frame3;
+    
+    btn3.center = CGPointMake(btn1.center.x, CGRectGetMaxY(btn2.frame) + 50);
     [self.view addSubview:btn3];
     [btn3 addTarget:self action:@selector(buttonTouched:) forControlEvents:UIControlEventTouchUpInside];
 }
@@ -72,13 +84,13 @@
 - (void)buttonTouched:(id)sender {
     UIButton *btn = (UIButton*) sender;
     NSString *labelText = btn.titleLabel.text;
-    NSArray *fortunes = @[@"Take advantage of an opportunity", @"Never quit!", @"Tomorrow is another day"];
+    NSArray *fortunes = @[@"Take advantage of an upcoming opportunity - make this really long so that it's more than 1 or 2 lines", @"Never quit!", @"Tomorrow is another day"];
     NSString *fortune = fortunes[btn.tag];
     self.lbl.text = [NSString stringWithFormat:@"%@: %@", labelText, fortune];
     [self.lbl sizeToFit];
     
     CGRect frame = self.lbl.frame;
-    frame.size.width = 320.0;
+    frame.size.width = 400.0;
     self.lbl.frame = frame;
 }
 
